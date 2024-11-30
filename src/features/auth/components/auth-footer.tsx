03977a68ce2f0871001a26123dde76eedc8b850f
AltruisticCraftLab <altruisticcraftlab@gmail.com>
2024-11-30 10:17:17 +0530

    Change the max width to a custom arbitrary value of 1920px & change the padding in the main Auth Layout

--- src/features/auth/components/auth-footer.tsx
+++ src/features/auth/components/auth-footer.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 export const AuthFooter = () => {
   return (
-    <div className="z-10 fixed bottom-0 left-1/2 -translate-x-1/2 max-w-screen-2xl w-full mx-auto flex items-center justify-center"> 
+    // <div className="z-10 fixed bottom-0 left-1/2 -translate-x-1/2 max-w-screen-2xl w-full mx-auto flex items-center justify-center"> 
+    <div className="z-10 fixed bottom-0 left-1/2 -translate-x-1/2 max-w-[1920px] w-full mx-auto flex items-center justify-center"> 
       <p className="text-xs md:text-base font-normal hover:opacity-50 pb-2 md:pb-4">
         Copyright © 2024, Any Platform. <span className="mr-5"></span> All Rights Reserved.
       </p>
--- src/features/auth/components/auth-navbar.tsx
+++ src/features/auth/components/auth-navbar.tsx
@@ -5,7 +5,8 @@ import { Logo } from "@/components/logo";
 
 export const AuthNavbar = () => {
   return (
-    <nav className="w-full mx-auto max-w-screen-2xl flex justify-between items-center">
+    // <nav className="w-full mx-auto max-w-screen-2xl flex justify-between items-center">
+    <nav className="w-full mx-auto max-w-[1920px] flex justify-between items-center">
       <Logo />
       <ModeToggle />
     </nav>
