afda97f05edf923a07be1ba3a0a602a48cb04b86
AltruisticCraftLab <altruisticcraftlab@gmail.com>
2024-11-30 08:33:40 +0530

    Change max-w-screen-lg to max-w-screen-xl in Auth Layout, Auth Navbar & Auth Footer.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ import { AuthNavbar } from "@/features/auth/components/auth-navbar";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
       <main className="min-h-screen">
-        <div className="mx-auto max-w-screen-lg p-4">
+        <div className="mx-auto max-w-screen-xl p-4">
           <AuthNavbar />  
           <div className="flex flex-col justify-center items-center pt-4 md:pt-14">
             {children}
