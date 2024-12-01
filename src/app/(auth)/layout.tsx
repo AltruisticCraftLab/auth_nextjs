da7b0267c9afb58b45e09e81ed487c9b3a05db60
AltruisticCraftLab <altruisticcraftlab@gmail.com>
2024-12-01 13:01:49 +0530

    Change max width back to 1024px for easy visibility of the Mode Toggle Button. ( More below )
    -- So now the user can change the theme from dark to light and vice versa easily
       as it is within easy visibility.
       Otherwise the user may not be able to see the mode toggle button on the auth page
       since the mode toggle button is at the extreme right of the page due to
       the width of the Auth page being large.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,8 +6,8 @@ import { AuthNavbar } from "@/features/auth/components/auth-navbar";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
       <main className="min-h-screen">
-        {/* <div className="mx-auto max-w-screen-2xl p-4"> */}
-        <div className="mx-auto max-w-[1920px] py-4 px-10">
+        <div className="mx-auto max-w-screen-lg p-4">
+        {/* <div className="mx-auto max-w-[1920px] py-4 px-10"> */}
           <AuthNavbar />  
           <div className="flex flex-col justify-center items-center pt-4 md:pt-14">
             {children}
